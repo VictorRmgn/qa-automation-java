706d4563e39e6930bfc0d12ad52cf35c3ab43798
VictorRmgn <ViktorR6543@yandex.ru>
2021-09-08 16:39:56 +0500

    Задание-5 Готовое с правками 2

--- src/main/java/com/tinkoff/edu/test/LoanCalcTest.java
+++ src/main/java/com/tinkoff/edu/test/LoanCalcTest.java
@@ -2,7 +2,7 @@ package com.tinkoff.edu.test;
 
 import com.tinkoff.edu.app.LoanCalcController;
 import com.tinkoff.edu.app.LoanRequest;
-import com.tinkoff.edu.app.LoanType;
+import static com.tinkoff.edu.app.LoanType.*;
 
 
 /**
@@ -10,7 +10,7 @@ import com.tinkoff.edu.app.LoanType;
  */
 public class LoanCalcTest {
     public static void main(String... args) {
-        LoanRequest request = new LoanRequest(LoanType.PERSON, 10, 2000);
+        LoanRequest request = new LoanRequest(PERSON, 10, 2000);
         LoanCalcController loanCalcController = new LoanCalcController();
 
         int requestId = loanCalcController.createRequest(request);
